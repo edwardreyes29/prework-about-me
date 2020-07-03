dda0246127bfc8d3e523d1d9d550602137f0dac1
Edward Reyes <reyes.edwardf@gmail.com>
2020-07-03 00:37:34 -0700

    finished modules 4 - 6

---
+++ module-6/javascript.js
@@ -0,0 +1,17 @@
+function grow() {
+    document.getElementById("box").style.height = "250px";
+}
+
+function changeToBlue() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fade() {
+    document.getElementById("box").style.opacity = 0.2;
+}
+
+function reset() {
+    document.getElementById("box").style.height= "150px";
+    document.getElementById("box").style.backgroundColor= "orange";
+    document.getElementById("box").style.opacity = 1;
+}
